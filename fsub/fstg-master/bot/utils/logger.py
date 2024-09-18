4a8d1cdb14653a25ff215ecb387894819a4858c2
minedeploy <174704366+minedeploy@users.noreply.github.com>
2024-09-18 04:39:05 +0000

    Update

--- fsub/fstg-master/bot/utils/logger.py
+++ fsub/fstg-master/bot/utils/logger.py
@@ -1,6 +1,4 @@
 import logging
-import os
-import sys
 from logging.handlers import RotatingFileHandler
 from typing import TYPE_CHECKING
 
@@ -26,14 +24,6 @@ class Logger:
         self.log_name = log_name
         self.log_setup()
 
-    def log_exception_handler(self, record) -> None:
-        # Restart the application on certain errors
-        exception_log = any(
-            key in record.name for key in {"asyncio", "pymongo"}
-        ) and record.levelname.strip() in {"WARN", "ERROR"}
-        if exception_log:
-            os.execvp(sys.executable, [sys.executable, *sys.argv])
-
     def log_setup(self) -> "logging.Logger":
         log_level = logging.INFO
 
@@ -52,21 +42,12 @@ class Logger:
         stream_handler.setFormatter(formatter)
         stream_handler.setLevel(log_level)
 
-        exception_handler = logging.StreamHandler()
-        exception_handler.setLevel(log_level)
-        exception_handler.setFormatter(formatter)
-        exception_handler.emit = self.log_exception_handler
-
         # Set up the basic configuration
-        logging.basicConfig(
-            level=log_level, handlers=[file_handler, stream_handler, exception_handler]
-        )
+        logging.basicConfig(level=log_level, handlers=[file_handler, stream_handler])
 
         self.log = logging.getLogger(self.log_name)
 
         # Reduce logging level for noisy libraries
-        logging.getLogger("apscheduler").setLevel(logging.WARNING)
-        logging.getLogger("httpx").setLevel(logging.WARNING)
         logging.getLogger("hydrogram").setLevel(logging.WARNING)
         logging.getLogger("pymongo").setLevel(logging.WARNING)
 
